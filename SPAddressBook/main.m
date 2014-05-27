8bbec759dc8e141cde52eee8f05033a4d215d40e
Kevin <kevinj04@gmail.com>
2014-05-26 22:00:00 -0400

    Initial Commit

---
+++ SPAddressBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SPAddressBook
+//
+//  Created by Kevin Jenkins on 5/26/14.
+//  Copyright (c) 2014 somethingpointless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
---
+++ SPAddressBookTests/SPAddressBookTests.m
@@ -0,0 +1,34 @@
+//
+//  SPAddressBookTests.m
+//  SPAddressBookTests
+//
+//  Created by Kevin Jenkins on 5/26/14.
+//  Copyright (c) 2014 somethingpointless. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SPAddressBookTests : XCTestCase
+
+@end
+
+@implementation SPAddressBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
